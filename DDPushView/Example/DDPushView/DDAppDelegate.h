638771db13574132fd6dcd2cc6fdc8a524f269df
dxjerry <35890052+dxjerry@users.noreply.github.com>
2021-12-07 16:04:31 +0800

    Add files via upload

---
+++ DDPushView/Example/DDPushView/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DDPushView
+//
+//  Created by dxjerry on 12/07/2021.
+//  Copyright (c) 2021 dxjerry. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DDPushView/Example/Pods/Target Support Files/Pods-DDPushView_Example/Pods-DDPushView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DDPushView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_DDPushView_Example
+@end
